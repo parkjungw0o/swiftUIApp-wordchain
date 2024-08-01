65cdef6e15645435e634278161ada2f6bb80292d
hotcat0421 <s24044@gsm.hs.kr>
2024-08-01 16:58:50 +0900

    Initial Commit

---
+++ WordChain/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordChain
+//
+//  Created by 박정우 on 8/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordChain/WordChainApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordChainApp.swift
+//  WordChain
+//
+//  Created by 박정우 on 8/1/24.
+//
+
+import SwiftUI
+
+@main
+struct WordChainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
